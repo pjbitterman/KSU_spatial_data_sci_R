48a64dace810954501aabe4a2cdc9dfe5d50ffab
Patrick Bitterman <patrick.bitterman@gmail.com>
2026-03-01 13:31:52 -0500

    week 08 ready

---
+++ src/tidycensus_scratch.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(tidycensus)
+library(sf)
+library(tmap)
+
+
+
+tidycensus::get_acs()
+
+
+oh <- get_acs(geography = "county", table = "B15003",
+                state = "OH", output = "wide", 
+              geometry = TRUE, year = 2020)
+
+oh.pop.c <- get_acs(geography = "county", table = "B01003",
+                                state = "OH", output = "wide", 
+                                geometry = F, year = 2020)
+
+
+
+oh.tract <- get_acs(geography = "tract", table = "B15003",
+                    state = "OH", output = "wide", 
+                    geometry = TRUE, year = 2020)
+
+oh.pop.t <- get_acs(geography = "tract", table = "B01003",
+                  state = "OH", output = "wide", 
+                  geometry = F, year = 2020)
+
+
+oh.joined <- oh.tract.t %>% left_join(., oh.pop, by = "GEOID") %>%
+  mutate(propBachelors = B15003_021M / B01003_001E)
+
+
+tmap::tmap_mode("plot")
+oh.joined %>% tm_shape(oh) + tm_polygons(fill = "propBachelors")
+
+
+
+oh.c.j <- oh %>% left_join(., oh.pop.c, by = "GEOID") %>%
+  mutate(propBachelors = B15003_021M / B01003_001E)
+
+oh.c.j %>% tm_shape(oh) + tm_polygons(fill = "propBachelors")
